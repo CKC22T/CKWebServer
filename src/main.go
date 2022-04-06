9bc63d80b9e89e93a3e4eb8ff6f9c44965161d34
winterkuma <equsejhy@naver.com>
2022-04-06 13:28:03 +0900

    feat (Web) : Room 시스템 및 프로파일링 시스템

--- src/main.go
+++ src/main.go
@@ -5,15 +5,22 @@ import (
 	"fmt"
 	"lobby"
 	"log"
+	"net"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/exec"
 	"room"
+	"runtime"
 	"strconv"
 	"syscall"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/websocket"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
 )
 
 type Address struct {
@@ -38,12 +45,17 @@ var dediCodeCount = 0
 var dediInitPort = 16000
 
 func main() {
+	room.SetIp()
 	http.HandleFunc("/login", lobby.LoginHandler)
 	http.HandleFunc("/signup", lobby.SignUpHandler)
 	http.HandleFunc("/getuserinfo", lobby.GetUserInfoHandler)
 	http.HandleFunc("/rooms", room.RoomsHandler)
-	http.HandleFunc("/run", ProcessHandler)
-	http.HandleFunc("/open", DedicatedProcessOnHandler)
+	http.HandleFunc("/user", lobby.GetUserInfoHandler)
+	//http.HandleFunc("/run", ProcessHandler)
+	http.HandleFunc("/open", room.DedicatedProcessOnEnd)
+
+	http.HandleFunc("/profile", ProfileHandler)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, world")
 	})
@@ -52,13 +64,111 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 }
 
-func ProcessHandler(w http.ResponseWriter, r *http.Request) {
-	process := exec.Command("../../Build/CKDedi/CKC2022.exe", strconv.Itoa(dediCodeCount))
-	process.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 16, NoInheritHandles: true}
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	runtimeOS := runtime.GOOS
+	// memory
+	vmStat, err := mem.VirtualMemory()
+	dealwithErr(err)
 
-	// process.SysProcAttr.CreationFlags = 16
-	// process.SysProcAttr.HideWindow = false
-	// process.SysProcAttr.NoInheritHandles = true
+	// disk - start from "/" mount point for Linux
+	// might have to change for Windows!!
+	// don't have a Window to test this out, if detect OS == windows
+	// then use "\" instead of "/"
+
+	diskStat, err := disk.Usage("/")
+	dealwithErr(err)
+
+	// cpu - get CPU number of cores and speed
+	cpuStat, err := cpu.Info()
+	dealwithErr(err)
+	percentage, err := cpu.Percent(0, true)
+	dealwithErr(err)
+
+	// host or machine kernel, uptime, platform Info
+	hostStat, err := host.Info()
+	dealwithErr(err)
+
+	// get interfaces MAC/hardware address
+	interfStat, err := net.Interfaces()
+	dealwithErr(err)
+
+	html := "<html>OS : " + runtimeOS + "<br>"
+	html = html + "Total memory: " + strconv.FormatUint(vmStat.Total, 10) + " bytes <br>"
+	html = html + "Free memory: " + strconv.FormatUint(vmStat.Free, 10) + " bytes<br>"
+	html = html + "Percentage used memory: " + strconv.FormatFloat(vmStat.UsedPercent, 'f', 2, 64) + "%<br>"
+
+	// get disk serial number.... strange... not available from disk package at compile time
+	// undefined: disk.GetDiskSerialNumber
+	//serial := disk.GetDiskSerialNumber("/dev/sda")
+
+	//html = html + "Disk serial number: " + serial + "<br>"
+
+	html = html + "Total disk space: " + strconv.FormatUint(diskStat.Total, 10) + " bytes <br>"
+	html = html + "Used disk space: " + strconv.FormatUint(diskStat.Used, 10) + " bytes<br>"
+	html = html + "Free disk space: " + strconv.FormatUint(diskStat.Free, 10) + " bytes<br>"
+	html = html + "Percentage disk space usage: " + strconv.FormatFloat(diskStat.UsedPercent, 'f', 2, 64) + "%<br>"
+
+	// since my machine has one CPU, I'll use the 0 index
+	// if your machine has more than 1 CPU, use the correct index
+	// to get the proper data
+	html = html + "CPU index number: " + strconv.FormatInt(int64(cpuStat[0].CPU), 10) + "<br>"
+	html = html + "VendorID: " + cpuStat[0].VendorID + "<br>"
+	html = html + "Family: " + cpuStat[0].Family + "<br>"
+	html = html + "Number of cores: " + strconv.FormatInt(int64(cpuStat[0].Cores), 10) + "<br>"
+	html = html + "Model Name: " + cpuStat[0].ModelName + "<br>"
+	html = html + "Speed: " + strconv.FormatFloat(cpuStat[0].Mhz, 'f', 2, 64) + " MHz <br>"
+
+	for idx, cpupercent := range percentage {
+		html = html + "Current CPU utilization: [" + strconv.Itoa(idx) + "] " + strconv.FormatFloat(cpupercent, 'f', 2, 64) + "%<br>"
+	}
+
+	html = html + "Hostname: " + hostStat.Hostname + "<br>"
+	html = html + "Uptime: " + strconv.FormatUint(hostStat.Uptime, 10) + "<br>"
+	html = html + "Number of processes running: " + strconv.FormatUint(hostStat.Procs, 10) + "<br>"
+
+	// another way to get the operating system name
+	// both darwin for Mac OSX, For Linux, can be ubuntu as platform
+	// and linux for OS
+
+	html = html + "OS: " + hostStat.OS + "<br>"
+	html = html + "Platform: " + hostStat.Platform + "<br>"
+
+	// the unique hardware id for this machine
+	html = html + "Host ID(uuid): " + hostStat.HostID + "<br>"
+
+	for _, interf := range interfStat {
+		html = html + "------------------------------------------------------<br>"
+		html = html + "Interface Name: " + interf.Name + "<br>"
+
+		if interf.HardwareAddr != nil {
+			html = html + "Hardware(MAC) Address: " + interf.HardwareAddr.String() + "<br>"
+		}
+
+		html = html + "Interface behavior of flags: " + interf.Flags.String() + "<br>"
+
+		addrs, _ := interf.Addrs()
+		for _, addr := range addrs {
+			html = html + "IPv6 or IPv4 addresses: " + addr.String() + "<br>"
+			//html = html + "IPv6 or IPv4 addresses: " + strconv.FormatInt(int64(addr), 10) + "<br>"
+		}
+
+	}
+
+	html = html + "</html>"
+
+	w.Write([]byte(html))
+}
+
+func dealwithErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		//os.Exit(-1)
+	}
+}
+
+func ProcessHandler(w http.ResponseWriter, r *http.Request) {
+	process := exec.Command("../../Build/ServerBuild/CKC2022.exe", strconv.Itoa(dediCodeCount))
+	process.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 16, NoInheritHandles: true}
 
 	err := process.Start()
 	if err != nil {
--- src/room/room.go
+++ src/room/room.go
@@ -3,17 +3,68 @@ package room
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"packet"
+	"strconv"
+	"syscall"
 )
 
-type Room struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	MaxUser int    `json:"maxUser"`
-	CurUser int    `json:"CurUser"`
+type Address struct {
+	Ip   string `json:"ip"`
+	Port int    `json:"port"`
 }
 
-var roomAtoi int = 0
+type DediProc struct {
+	Proc *os.Process
+	Id   int
+	IsOn chan bool
+	Addr Address
+}
+
+type IP struct {
+	Query string
+}
+
+func SetIp() {
+	req, err := http.Get("http://ip-api.com/json/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer req.Body.Close()
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var ip IP
+	json.Unmarshal(body, &ip)
+
+	dediServerIp = ip.Query
+}
+
+var dediServerIp string
+var dediServers = map[int]*DediProc{}
+var dediCodeCount = 1
+var dediInitPort = 16000
+
+type Room struct {
+	Id      int     `json:"id"`
+	Name    string  `json:"name"`
+	MaxUser int     `json:"maxUser"`
+	CurUser int     `json:"curUser"`
+	Addr    Address `json:"addr"`
+}
+
+type RoomInfoRes struct {
+	Err      int  `json:"err"`
+	RoomInfo Room `json:"roomInfo"`
+}
+
 var rooms = map[int]*Room{}
 
 func RoomsHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,23 +73,92 @@ func RoomsHandler(w http.ResponseWriter, r *http.Request) {
 		var room Room
 		json.NewDecoder(r.Body).Decode(&room)
 		fmt.Println(room.Id)
-		json.NewEncoder(w).Encode(rooms)
+		if room.Id > 0 {
+			if rooms[room.Id] != nil {
+				var roomInfo RoomInfoRes
+				roomInfo.Err = packet.Unknown
+				json.NewEncoder(w).Encode(roomInfo)
+			} else {
+				var roomInfo RoomInfoRes
+				roomInfo.Err = packet.Success
+				roomInfo.RoomInfo = *rooms[room.Id]
+				json.NewEncoder(w).Encode(roomInfo)
+			}
+		} else {
+			json.NewEncoder(w).Encode(rooms)
+		}
 	case http.MethodPost:
 		var room Room
 		json.NewDecoder(r.Body).Decode(&room)
-		room.Id = roomAtoi
+		dediProc := DedicatedProcessOnBegin()
+		room.Id = dediProc.Id
+		room.Addr = dediProc.Addr
+		room.MaxUser = 4
+		room.CurUser = 0
 		rooms[room.Id] = &room
-		roomAtoi = roomAtoi + 1
-		json.NewEncoder(w).Encode(room)
+		var roomInfo RoomInfoRes
+		roomInfo.Err = packet.Success
+		roomInfo.RoomInfo = room
+		json.NewEncoder(w).Encode(roomInfo)
 	case http.MethodPut:
 		var room Room
 		json.NewDecoder(r.Body).Decode(&room)
-		rooms[room.Id] = &room
-		json.NewEncoder(w).Encode(room)
+		rooms[room.Id].CurUser = room.CurUser
+		var roomInfo RoomInfoRes
+		roomInfo.Err = packet.Success
+		roomInfo.RoomInfo = *rooms[room.Id]
+		json.NewEncoder(w).Encode(roomInfo)
 	case http.MethodDelete:
 		var room Room
 		json.NewDecoder(r.Body).Decode(&room)
+		DedicatedProcessKill(room.Id)
 		delete(rooms, room.Id)
 		json.NewEncoder(w).Encode(room)
 	}
+}
+
+func DedicatedProcessOnBegin() *DediProc {
+	process := exec.Command("../../Build/ServerBuild/CKC2022.exe", strconv.Itoa(dediCodeCount))
+	process.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 16, NoInheritHandles: true}
+
+	err := process.Start()
+	if err != nil {
+		log.Println(err)
+	}
+	dediProc := &DediProc{Proc: process.Process, Id: dediCodeCount, Addr: Address{dediServerIp, dediInitPort + dediCodeCount}}
+	dediServers[dediCodeCount] = dediProc
+	dediCodeCount = dediCodeCount + 1
+
+	dediProc.IsOn = make(chan bool)
+	isOn := <-dediProc.IsOn
+	if isOn {
+		log.Printf("[Dedi] Dedicated Server On : ID[%v] IP[%v] Port[%v]", dediProc.Id, dediProc.Addr.Ip, dediProc.Addr.Port)
+	} else {
+		log.Println("[Error] Dedicated Server Not Open")
+	}
+
+	return dediProc
+}
+
+func DedicatedProcessOnEnd(w http.ResponseWriter, r *http.Request) {
+	var addr Address
+	json.NewDecoder(r.Body).Decode(&addr)
+	var procId = addr.Port - dediInitPort
+	var result bool = true
+	if dediServers[procId] != nil {
+		dediServers[procId].IsOn <- true
+	} else {
+		var room Room
+		room.Id = 100 + procId
+		room.Addr = addr
+		room.MaxUser = 4
+		room.CurUser = 0
+		rooms[room.Id] = &room
+		result = false
+	}
+	json.NewEncoder(w).Encode(result)
+}
+
+func DedicatedProcessKill(id int) {
+	dediServers[id].Proc.Kill()
 }
