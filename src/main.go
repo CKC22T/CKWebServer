8e66ba5244bfeb44da5cfae24bb60af9137d6a0d
WinterKuma <equsejhy@naver.com>
2022-05-02 20:46:38 +0900

    fix (Download) : url 다운로드 제작

--- src/main.go
+++ src/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"lobby"
 	"net/http"
 	_ "net/http/pprof"
 	"profile"
 	"room"
+	"strconv"
+	"time"
 	"websocket"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -31,5 +35,37 @@ func main() {
 		websocket.ServerWs(hub, w, r)
 	})
 
+	http.HandleFunc("/download", ForceDownload)
+
 	http.ListenAndServe(":3000", nil)
+}
+
+func ForceDownload(w http.ResponseWriter, r *http.Request) {
+	file := "ClientBuild.zip"
+	downloadBytes, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// set the default MIME type to send
+	mime := http.DetectContentType(downloadBytes)
+
+	fileSize := len(string(downloadBytes))
+
+	// Generate the server headers
+	w.Header().Set("Content-Type", mime)
+	w.Header().Set("Content-Disposition", "attachment; filename="+file+"")
+	w.Header().Set("Expires", "0")
+	w.Header().Set("Content-Transfer-Encoding", "binary")
+	w.Header().Set("Content-Length", strconv.Itoa(fileSize))
+	w.Header().Set("Content-Control", "private, no-transform, no-store, must-revalidate")
+
+	//b := bytes.NewBuffer(downloadBytes)
+	//if _, err := b.WriteTo(w); err != nil {
+	//              fmt.Fprintf(w, "%s", err)
+	//      }
+
+	// force it down the client's.....
+	http.ServeContent(w, r, file, time.Now(), bytes.NewReader(downloadBytes))
 }
